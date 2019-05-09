eede02b566f8adce43f2350e43921d23f8f9d4c6
Jason Thompson <jason99.thompson@gmail.com>
2019-05-09 16:20:40 +0100

    Bug fixes and added overall_summary

--- tcx.py
+++ tcx.py
@@ -62,7 +62,8 @@ class tcx(object):
                                          'prev_tim' : datetime.datetime.strptime('1970-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'),
                                          'prev_lat' : 0.0,
                                          'prev_lon' : 0.0,
-                                         'prev_alt' : 0.0}):  
+                                         'prev_alt' : 0.0,
+                                         'prev_gcum_dis' : 0.0}):  
                   
         """
         Extract time, lat, long, altitude and heartrate at each trackpoint.
@@ -75,6 +76,9 @@ class tcx(object):
         Also need to deal with cases where the GPS has not kicked in and therefore no lat lon.
         """
         
+        # if first point of file_name then need to treat differently
+        first_point = True
+        
         tcx_file = ET.parse(file_name)    
         
         # initialise in particular set the previous trackpoint which defaults to zero if first one 
@@ -83,7 +87,8 @@ class tcx(object):
         prev_tim = previous_trackpoint['prev_tim']
         prev_lat = previous_trackpoint['prev_lat']
         prev_lon = previous_trackpoint['prev_lon']
-        prev_alt = previous_trackpoint['prev_alt']        
+        prev_alt = previous_trackpoint['prev_alt']
+        prev_gcum_dis = previous_trackpoint['prev_gcum_dis']
             
         root = tcx_file.getroot()
     
@@ -91,6 +96,7 @@ class tcx(object):
         # We'll just use `tcx:` and the short name instead.
         ns = {
             'tcx': 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2',
+            'ns3' : 'http://www.garmin.com/xmlschemas/ActivityExtension/v2'
         }
     
         trackpoints = root.findall('./tcx:Activities/tcx:Activity/tcx:Lap/tcx:Track/tcx:Trackpoint', ns)
@@ -125,6 +131,17 @@ class tcx(object):
                 bpm = int(trackpoint.find('./tcx:HeartRateBpm/tcx:Value', ns).text)
             except:
                 data_issue = True
+                
+            try:
+                gcum_dis = float(trackpoint.find('./tcx:DistanceMeters', ns).text)
+            except:
+                gcum_dis = -99.0
+
+            try:                
+                gspeed = float(trackpoint.findall('./tcx:Extensions/ns3:TPX/ns3:Speed', 
+                                                  ns)[0].text)
+            except:
+                data_issue = True
             
             # ignore trackpoints where any of the attributes are missing
             if data_issue:
@@ -136,7 +153,7 @@ class tcx(object):
                     # data is same as previous trackpoint so ignore
                     pass
                 else:
-                    if prev_lat > 0:
+                    if not first_point:
                         # not the first trackpoint
                         dis = self.distance((prev_lat, prev_lon), (lat, lon))
                         cum_dis += dis
@@ -151,34 +168,60 @@ class tcx(object):
                         prev_alt = alt
     
                         speed = 3600 * dis / dur  
-                        alt_pc = 100 * (alt_diff / (dis * 1000))
-    
-                        data.append([dis, cum_dis, tim, dur, cum_dur, 
-                                     lat, lon, alt, alt_diff, alt_pc, bpm, speed])  
+                        if dis == 0:
+                            alt_pc = 0
+                        else:
+                            alt_pc = 100 * (alt_diff / (dis * 1000))
+                        
+                        # Garmin distance in meters
+                        # convert to km
+                        gcum_dis /= 1000
+                        
+                        gdis = gcum_dis - prev_gcum_dis
+                        prev_gcum_dis = gcum_dis                        
+                        
+                        
+                        # Garmin speed measured in meters per second
+                        # convert to km per hr
+                        gspeed *= (3600 / 1000)                         
+                        
+                        data.append([dis, gdis, cum_dis, gcum_dis, 
+                                     tim, dur, cum_dur, 
+                                     lat, lon, alt, alt_diff, alt_pc, bpm, 
+                                     speed, gspeed])  
                     else:
                         # first trackpoint 
+                        first_point = False                        
                         prev_tim = tim
                         prev_lat = lat
                         prev_lon = lon
                         prev_alt = alt
-    
-        data_df = pd.DataFrame(data, columns=['dis', 'cum_dis', 'tim', 'dur', 'cum_dur', 
-                                              'lat', 'lon', 'alt', 'alt_diff', 'alt_pc', 'bpm', 'speed'])
+                        prev_gcum_dis = gcum_dis
+                            
+        data_df = pd.DataFrame(data, columns=['dis', 'gdis', 'cum_dis', 'gcum_dis', 
+                                              'tim', 'dur', 
+                                              'cum_dur', 'lat', 'lon', 'alt', 
+                                              'alt_diff', 'alt_pc', 'bpm', 
+                                              'speed', 'gspeed'])
         
         last_trackpoint = {'cum_dur' : cum_dur,
                            'cum_dis' : cum_dis,
                            'prev_tim' : tim,
                            'prev_lat' : lat,
                            'prev_lon' : lon,
-                           'prev_alt' : alt}
+                           'prev_alt' : alt,
+                           'prev_gcum_dis' : gcum_dis}
     
         return data_df, last_trackpoint    
     
     
     def gradient_summary(self, data_df, zero_speed_threshold, data_label, 
-                         gradient_label, min_grad, max_grad, output=True):
+                         gradient_label, min_grad, max_grad, output=True,
+                         pa_stop_speed=25, garmin_speed=True):
         """
         Summarise the data for a specific gradient range (measured as a ratio)
+        If garmin_speed=True then use the Garmin speed at each point to decide
+        if point is zero etc otherwise use calculated.
         
         data_df: has the data
         zero_speed_threshold: anything below this speed (kph) reported seperately
@@ -187,10 +230,16 @@ class tcx(object):
         min_grad, max_grad: range of gradients to summarise
         output: printed summary required?        
         """
+        
+        if garmin_speed:
+            speed_col_name = 'gspeed'
+        else:
+            speed_col_name = 'speed'
     
+        
         totals = data_df.sum()
-        zero_totals = data_df[data_df['speed'] < zero_speed_threshold].sum()
-        nonzero_totals = data_df[data_df['speed'] >= zero_speed_threshold].sum()
+        zero_totals = data_df[data_df[speed_col_name] < zero_speed_threshold].sum()
+        nonzero_totals = data_df[data_df[speed_col_name] >= zero_speed_threshold].sum()
         
         
         x_totals = data_df[(data_df['alt_pc'] <= max_grad) &
@@ -198,15 +247,19 @@ class tcx(object):
     
         zero_totals = data_df[(data_df['alt_pc'] <= max_grad) &
                                 (data_df['alt_pc'] > min_grad) &
-                                (data_df['speed'] < zero_speed_threshold)].sum()
+                                (data_df[speed_col_name] < zero_speed_threshold)].sum()
+        
+        no_assist_totals = data_df[(data_df['alt_pc'] <= max_grad) &
+                        (data_df['alt_pc'] > min_grad) &
+                        (data_df[speed_col_name] > pa_stop_speed)].sum()
     
         nonzero_totals = data_df[(data_df['alt_pc'] <= max_grad) &
                                    (data_df['alt_pc'] > min_grad) & 
-                                   (data_df['speed'] >= zero_speed_threshold)].sum()
+                                   (data_df[speed_col_name] >= zero_speed_threshold)].sum()
         
         nonzero_means = data_df[(data_df['alt_pc'] <= max_grad) &
                                 (data_df['alt_pc'] > min_grad) & 
-                                (data_df['speed'] >= zero_speed_threshold)].mean()
+                                (data_df[speed_col_name] >= zero_speed_threshold)].mean()
         
         if nonzero_totals['dur'] > 0:
             speed = nonzero_totals['dis'] / (nonzero_totals['dur'] / 3600.0)
@@ -215,7 +268,7 @@ class tcx(object):
     
         bpm_data_df = data_df[(data_df['alt_pc'] <= max_grad) &
                                 (data_df['alt_pc'] > min_grad) & 
-                                (data_df['speed'] >= zero_speed_threshold)].loc[:, ['dur', 'bpm']]
+                                (data_df[speed_col_name] >= zero_speed_threshold)].loc[:, ['dur', 'bpm']]
         if bpm_data_df.shape[0] > 0:
             avg_bpm = np.average(bpm_data_df['bpm'], weights=bpm_data_df['dur'])
         else:
@@ -226,11 +279,43 @@ class tcx(object):
         else:
             dur_pc = 0
         
-        return_df = pd.DataFrame([[speed, avg_bpm, dur_pc]], 
+        if totals['dis'] > 0:
+            dis_pc = nonzero_totals['dis'] / totals['dis'] 
+        else:
+            dis_pc = 0        
+        
+        if x_totals['dur'] > 0:
+            zero_speed_pc = zero_totals['dur'] / x_totals['dur']
+            non_assist_pc = no_assist_totals['dur'] / x_totals['dur']
+            assist_pc = 1 - (zero_speed_pc + non_assist_pc)
+        else:
+            zero_speed_pc = 0
+            non_assist_pc = 0
+            assist_pc = 0
+            
+        if x_totals['dis'] > 0:
+            zero_speed_dis_pc = zero_totals['dis'] / x_totals['dis']
+            non_assist_dis_pc = no_assist_totals['dis'] / x_totals['dis']
+            assist_dis_pc = 1 - (zero_speed_dis_pc + non_assist_dis_pc)
+        else:
+            zero_speed_dis_pc = 0
+            non_assist_dis_pc = 0
+            assist_dis_pc = 0
+        
+        return_df = pd.DataFrame([[speed, avg_bpm, dur_pc, dis_pc,
+                                   zero_speed_pc, assist_pc, non_assist_pc,
+                                   zero_speed_dis_pc, assist_dis_pc, non_assist_dis_pc]], 
                                  index=[gradient_label],
                                  columns=['speed_' + data_label, 
                                           'bpm_' + data_label,
-                                          'dur_' + data_label])
+                                          'dur_' + data_label,
+                                          'dis_' + data_label,
+                                          'zeropc_' + data_label,
+                                          'assistpc_' + data_label,
+                                          'nonpc_' + data_label,
+                                          'dis_zeropc_' + data_label,
+                                          'dis_assistpc_' + data_label,
+                                          'dis_nonpc_' + data_label])
                 
         if output:
         
@@ -256,9 +341,56 @@ class tcx(object):
         return return_df
     
     
+    def overall_summary(self, data_df, zero_speed_threshold, pa_stop_speed):
+        """
+        Summarise the data in data_df:
+            avg speed (km per hr)
+            total distance (km)
+            total time (km)
+            total ascent (m)
+            total descent (m)
+            total and percent of time less than zero_speed_threshold
+            total and percent of time greater than pa_stop_speed
+        """
     
+        tot_dis = data_df.iloc[-1, :]['cum_dis']
+        tot_dur = data_df.iloc[-1, :]['cum_dur']
+        avg_speed = 3600 * tot_dis / tot_dur
+        
+        tot_asc = data_df[data_df['alt_diff'] > 0]['alt_diff'].sum()
+        tot_desc = data_df[data_df['alt_diff'] < 0]['alt_diff'].sum()
+        
+        zero_dur_pc = (data_df[data_df['speed'] < zero_speed_threshold]['dur'].sum() /
+                       tot_dur)
+        zero_dis_pc = (data_df[data_df['speed'] < zero_speed_threshold]['dis'].sum() /
+                       tot_dis)                    
+        
+        nonpa_dur_pc = (data_df[data_df['speed'] > pa_stop_speed]['dur'].sum() /
+                       tot_dur)
+        nonpa_dis_pc = (data_df[data_df['speed'] > pa_stop_speed]['dis'].sum() /
+                       tot_dis)
+
+        pa_dur_pc = 1 - (zero_dur_pc + nonpa_dur_pc)
+        pa_dis_pc = 1 - (zero_dis_pc + nonpa_dis_pc)  
+
+        return_dic = {'tot_dis' : tot_dis,
+                      'tot_dur' : tot_dur,
+                      'avg_speed' : avg_speed,
+                      'tot_asc' : tot_asc,
+                      'tot_desc' : tot_desc,
+                      'zero_dur_pc' : zero_dur_pc,
+                      'zero_dis_pc' : zero_dis_pc,
+                      'pa_dur_pc' : pa_dur_pc,
+                      'pa_dis_pc' : pa_dis_pc,
+                      'nonpa_dur_pc' : nonpa_dur_pc,
+                      'nonpa_dis_pc' : nonpa_dis_pc}
+        
+        return return_dic
+
+
+
     def lap_summary(data_df, interval, zero_speed_threshold, 
-                    speed_zones, flat_gradient=0.02
+                    speed_zones, flat_gradient=0.02,
                     start=True):
         """
         Produce summary stats for each lap where a lap is based upon
